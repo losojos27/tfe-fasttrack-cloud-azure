338911e4b5496da473ff235dce3ff2f7964f56d6
Lee Owensby <lee.owensby@gmail.com>
2022-12-14 13:03:20 -0600

    adding variables.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,15 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  name     = var.resource_group_name
   location = "westus2"
 }
+
+# # Create a virtual network
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "myTFVnet"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = "westus2"
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
+
